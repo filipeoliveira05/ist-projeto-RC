70805d3b9f55491634a3f0d0c37def71d6d1f8c2
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2025-12-19 08:41:58 +0000

    revert commit

--- RC2526P1/server_logic.h
+++ RC2526P1/server_logic.h
@@ -6,7 +6,7 @@
 #include <netinet/in.h>
 
 // Processa um pedido UDP completo
-void process_udp_request(int udp_fd, struct sockaddr_in *client_addr, char *buffer, bool verbose);
+void process_udp_request(int udp_fd, struct sockaddr_in *client_addr, char *buffer, ServerState *server_data, bool verbose);
 
 // Processa um pedido TCP completo
 void process_tcp_request(int client_fd, char *buffer, ssize_t buffer_size, ServerState *server_data, bool verbose, char *response_buffer, int response_size);
